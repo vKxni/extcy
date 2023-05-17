68f157684bb51e9e740aa9682fe94a4e11ea87bf
Koni vKxni <eventbeate@gmail.com>
2023-05-17 21:47:24 +0200

    exports

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+// enum
+export * from "./functions/enum/chunkBy";
+export * from "./functions/enum/chunkEvery";
+export * from "./functions/enum/chunkWhile";
+export * from "./functions/enum/concat";
+export * from "./functions/enum/countUntil";
+export * from "./functions/enum/dedup";
+export * from "./functions/enum/deleteItem";
+export * from "./functions/enum/dropEvery";
+export * from "./functions/enum/dropWhile";
+export * from "./functions/enum/each";
+export * from "./functions/enum/findIndex";
+export * from "./functions/enum/findValue";
+export * from "./functions/enum/flatMapReduce";
+export * from "./functions/enum/groupBy";
+export * from "./functions/enum/into";
+export * from "./functions/enum/introsperse";
+export * from "./functions/enum/mapEvery";
+export * from "./functions/enum/mapFetch";
+export * from "./functions/enum/mapReduce";
+export * from "./functions/enum/maxBy";
+export * from "./functions/enum/minMaxBy";
+export * from "./functions/enum/reverseSlice";
+export * from "./functions/enum/scan";
+export * from "./functions/enum/shuffle";
+export * from "./functions/enum/sortBy";
+export * from "./functions/enum/sum";
+export * from "./functions/enum/takeEvery";
+export * from "./functions/enum/takeRandom";
+export * from "./functions/enum/uniq";
+export * from "./functions/enum/uniqueBy";
+export * from "./functions/enum/unzip";
+export * from "./functions/enum/withIndex";
+export * from "./functions/enum/zipReduce";
+export * from "./functions/enum/zipWith";
+
+// list
+export * from "./functions/list/asciiPrintable";
+export * from "./functions/list/duplicate";
+export * from "./functions/list/flattenList";
+export * from "./functions/list/foldl";
+export * from "./functions/list/foldr";
+export * from "./functions/list/frequencies";
+export * from "./functions/list/fromStruct";
+export * from "./functions/list/keyDelete";
+export * from "./functions/list/keyFind";
+export * from "./functions/list/keyMember";
+export * from "./functions/list/keySort";
+export * from "./functions/list/keyStore";
+export * from "./functions/list/myersDifferance";
+export * from "./functions/list/popAt";
+export * from "./functions/list/updateAt";
+export * from "./functions/list/wrap";
+
+// map
+export * from "./functions/map/getAndUpdate";
+export * from "./functions/map/getLazy";
+export * from "./functions/map/mapEqual";
+export * from "./functions/map/mapPutNewLazy";
+export * from "./functions/map/mapValues";
+export * from "./functions/map/popLazy";
+export * from "./functions/map/reject";
+export * from "./functions/map/update";
+
+// guards
+export * from "./guards/unless";
+
+// operations
+export * from "./operations/cond";
+export * from "./operations/match";
+export * from "./operations/when";
+
+// utility
+export * from "./utility/pipe";
+export * from "./utility/sigil";
